70b24257a12fafb9ee5bd1834165e2f8d6ddd0e5
Nikolaos Maragkos <nik.maragos@gmail.com>
2025-03-03 14:27:30 +0200

    First release version

--- control_gen.py
+++ control_gen.py
@@ -1,28 +1,59 @@
 import os
 
+import maya.cmds as mc
 import maya.OpenMayaUI as omui
 import pymel.core as pm
-from PySide6 import QtCore, QtUiTools, QtWidgets
+from PySide6.QtCore import QFile, Qt
+from PySide6.QtGui import QAction, QPixmap
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QDialog, QListWidget, QListWidgetItem, QMainWindow, QMenu, QVBoxLayout
 from shiboken6 import wrapInstance
 
 import control_gen_api as cgen
+import constants
 from logger.logger import Logger
 
-
 logger = Logger()
+logger.set_level("INFO")
 logger.set_propagate(False)
 
 
-class ControllerLibrary(QtWidgets.QDialog):
+class CustomListWidget(QListWidget):
+    """A customised version of QListWidget that allows for context menus.
+    It will be used to substitute the original QListWidget in the .ui file.
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def contextMenuEvent(self, event):
+        menu = QMenu(self)
+        delete_action = QAction("Delete", self)
+        menu.addAction(delete_action)
+
+        delete_action.triggered.connect(lambda: self.delete_item())
+
+        menu.exec_(event.globalPos())
+
+    def delete_item(self):
+        item = self.currentItem()
+        if item:
+            cgen.delete_controller(item.text())
+            self.takeItem(self.row(item))
+
+
+class ControllerLibrary(QDialog):
     def __init__(self, parent=None):
         super(ControllerLibrary, self).__init__(parent)
 
-        loader = QtUiTools.QUiLoader()
+        loader = QUiLoader()
         current_dir = os.path.dirname(__file__)
-        ui_filepath = QtCore.QFile(os.path.join(current_dir, "control_gen.ui"))
-        ui_filepath.open(QtCore.QFile.OpenModeFlag.ReadOnly)
+        ui_filepath = QFile(os.path.join(current_dir, "control_gen.ui"))
+        ui_filepath.open(QFile.OpenModeFlag.ReadOnly)
         self.ui = loader.load(ui_filepath, parentWidget=self)
         ui_filepath.close()
+
+        if not self.ui:
+            raise RuntimeError(loader.errorString())
 
         self.init_ui()
         self.set_signals()
@@ -33,7 +64,7 @@ class ControllerLibrary(QtWidgets.QDialog):
             parent_height = parent.height()
 
             ui_width = 250
-            ui_height = 375
+            ui_height = 650
 
             pos_x = parent_width * 0.5 - ui_width * 0.5 + parent.x()
             pos_y = parent_height * 0.5 - ui_height * 0.5 + parent.y()
@@ -41,35 +72,80 @@ class ControllerLibrary(QtWidgets.QDialog):
             self.ui.setGeometry(pos_x, pos_y, ui_width, ui_height)
 
     def init_ui(self):
+        self.controlListWidget = CustomListWidget(self.ui.controlListWidget.parent())
+        layout = self.ui.findChild(QVBoxLayout, "verticalLayout")
+        layout.addWidget(self.controlListWidget)
+        original_list_widget = self.ui.findChild(QListWidget, "controlListWidget")
+        layout.removeWidget(original_list_widget)
+        original_list_widget.deleteLater()
+
         self.update_control_list_widget()
+        self.set_controller_colour(0)
+        self.load_controller_icon()
 
     def set_signals(self):
-        self.ui.controlListWidget.itemDoubleClicked.connect(self.import_controller)
-        self.ui.scaleSlider.valueChanged[int].connect(self.set_controller_scale)
-        self.ui.scaleValLineEdit.textChanged[str].connect(self.update_scale_slider)
+        self.controlListWidget.itemDoubleClicked.connect(self.import_controller)
+        self.controlListWidget.itemClicked.connect(self.replace_controller_name)
+        self.controlListWidget.itemClicked.connect(self.load_controller_icon)
+        self.ui.scaleSlider.valueChanged.connect(self.set_controller_scale)     #int
+        self.ui.scaleValLineEdit.textChanged.connect(self.update_scale_slider)  #str
         self.ui.saveControlButton.clicked.connect(self.export_controller)
+        self.ui.colourSwatchSlider.valueChanged.connect(self.set_controller_colour) #int
+        self.ui.controlNameLineEdit.editingFinished.connect(self.controlListWidget.clearSelection)
+
+    def load_controller_icon(self, icon=""):
+        """Load an image relative to the conroller selected, on a QLabel.
+
+        Args:
+            icon (str, QListWidgetItem): The controller name to match to the controller icon.
+        """
+        icon_name = icon
+        if isinstance(icon, QListWidgetItem):
+            icon_name = icon.text()
+
+        icon_filepath = os.path.join(constants.ICONS_FOLDER, f"{icon_name}.png")
+        
+        if not os.path.isfile(icon_filepath):
+            icon_filepath = constants.NO_ICON
+
+        pixmap = QPixmap(icon_filepath).scaled(
+            self.ui.iconLabel.size(),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
+        )
+        self.ui.iconLabel.setPixmap(pixmap)
+
+    def replace_controller_name(self, select_control):
+        """Update the controller name on the QLineEdit box, when one is selected from the list."""
+        self.ui.controlNameLineEdit.setText(select_control.text())
 
     def update_control_list_widget(self):
+        """Refresh the contents of the controllers' list."""
         control_list = cgen.controller_list()
-        self.ui.controlListWidget.clear()
+        self.controlListWidget.clear()
         for control_item in control_list:
-            item = QtWidgets.QListWidgetItem(control_item)
-            self.ui.controlListWidget.addItem(item)
+            item = QListWidgetItem(control_item)
+            self.controlListWidget.addItem(item)
 
-    def import_controller(self):
-        """Import controller from library"""
-        import_control_name = self.sender().currentItem().text()
-        cgen.generate_controller(
-            import_control_name,
-            float(self.ui.scaleValLineEdit.text())
+    def import_controller(self, controller):
+        """Import the selected controller into Maya.
+        
+        Args:
+            controller (QListWidgetItem): The item selected from the list of controllers.
+        """
+        cgen.load_controller(
+            controller.text(),
+            float(self.ui.scaleValLineEdit.text()),
+            colour=self.ui.colourSwatchSlider.value()
         )
     
     def export_controller(self):
         """Save selected curve to controllers library."""
-        selection = pm.selected()[0]
-
-        if not selection:
+        try:
+            selection = pm.selected()[0]
+        except:
             logger.warning("Nothing selected")
+            mc.confirmDialog(title="Error", message="No curve selected!", icon="critical", button=["Cancel"])
             return
 
         if selection.getShape().nodeType() != "nurbsCurve":
@@ -82,25 +158,52 @@ class ControllerLibrary(QtWidgets.QDialog):
             logger.error("Please enter a name for the new controller...")
             return
         
-        cgen.save_controller(curve=selection, curve_name=curve_name)
+        cgen.save_controller(curve=selection, curve_name=curve_name, create_icon=True)
 
         self.update_control_list_widget()
+        self.load_controller_icon(curve_name)
 
     def set_controller_scale(self, value):
+        """Set a scale value for the controller to be imported.
+        The amount needs to be passed as multiples of ten to accomodate for decimal values in the ui.
+        
+        Args:
+            value (int): The scale amount.
+        """
         scale_value = float(value)/10.0
         self.ui.scaleValLineEdit.setText(str(scale_value))
 
     def update_scale_slider(self, value):
-        """Update the scale slider position, when a scale value is manually entered."""
+        """Update the scale slider position, when a scale value is manually entered.
+        
+        Args:
+            value (float): The scaling amount, manually entered.
+        """
         if value:
             self.ui.scaleSlider.setValue(float(value) * 10)
+
+    def set_controller_colour(self, colour):
+        """Set the colour of the controller to be imported.
+        The actual colour value corresponds to Maya's colour numbers for overrides
+        and is converted to an RGB representation to be shown in the ui.
+        
+        Args:
+            colour (int): The colour value from the relative slider.
+        """
+        if colour:
+            rgb_colour = ",".join(
+                str(color) for color in constants.maya_colour_map[colour]
+            )
+            self.ui.colourSwatchLabel.setStyleSheet(f"background-color:rgb({rgb_colour})")
+        else:
+            self.ui.colourSwatchLabel.setStyleSheet("")
 
 
 def get_maya_window():
     """Pointer to the Maya main window"""
     ptr = omui.MQtUtil.mainWindow()
     if ptr:
-        return wrapInstance(int(ptr), QtWidgets.QMainWindow)
+        return wrapInstance(int(ptr), QMainWindow)
 
 
 def run():
--- control_gen_api.py
+++ control_gen_api.py
@@ -2,34 +2,43 @@ import os
 
 import pickle
 import pymel.core as pm
+import maya.OpenMaya as om
+import maya.OpenMayaUI as omui
+from PIL import Image
 
+import constants
 from logger.logger import Logger
 
-
-DEFAULT_LIB_PATH = os.path.join(os.path.dirname(__file__), "cons_pickle.pkl")
-
 logger = Logger()
+logger.set_level("INFO")
 logger.set_propagate(False)
 
 try:
-    with open(DEFAULT_LIB_PATH, "rb") as file:
+    with open(constants.DEFAULT_LIB_PATH, "rb") as file:
         controllers_dict = pickle.load(file)
         logger.debug(controllers_dict)
-        file.close()
 except:
-    logger.debug(f".pkl file not found ({DEFAULT_LIB_PATH}). Initialising empty 'controllers_dict'...")
+    logger.debug(f".pkl file not found ({constants.DEFAULT_LIB_PATH}). Initialising empty 'controllers_dict'...")
     controllers_dict={}
 
 
-def save_controller(curve=None, curve_name=None):
-    """Saves selected NURBS curve (controller) to the pickle file"""
+def save_controller(curve=None, curve_name=None, create_icon=False):
+    """Saves selected NURBS curve (controller) to the library.
+    
+    Args:
+        curve (nt.Transform): The Maya curve node to be saved in the library.
+        curve_name (str): The name of the controller
+        create_icon (bool): Whether or not to create a corresponding icon file.
+    """
     global controllers_dict
 
     controller = curve or pm.selected()[0]
+    if not controller:
+        return
+    
     controller_shape = controller.getShape()
 
     controller_name = curve_name or pm.promptBox("Controller name", "Please enter a name:", "Ok", "Cancel")
-
     if not controller_name:
         return
     
@@ -42,14 +51,42 @@ def save_controller(curve=None, curve_name=None):
         "knots": controller_shape.getKnots(),
         "spans": controller_shape.spans.get(),        
     }
+
+    if create_icon:
+        viewport_snip(controller_name)
     
-    with open(DEFAULT_LIB_PATH, "wb") as file:
+    with open(constants.DEFAULT_LIB_PATH, "wb") as file:
         pickle.dump(controllers_dict, file)
-        file.close()
 
+def delete_controller(controller_name):
+    """Delete a controller from the library.
 
-def generate_controller(controller_name=None, scale=1.0):
-    """Generate a NURBS curve controller"""
+    Args:
+        controller_name (str): The name of the controller to be removed.    
+    """
+    with open(constants.DEFAULT_LIB_PATH, "rb") as file:
+        controllers_dict = pickle.load(file)
+    
+    if controller_name in controllers_dict:
+        del controllers_dict[controller_name]
+        with open(constants.DEFAULT_LIB_PATH, "wb") as file:
+            pickle.dump(controllers_dict, file)
+
+        icon_filepath = os.path.join(constants.ICONS_FOLDER, f"{controller_name}.png")
+        if os.path.isfile(icon_filepath):
+            os.remove(icon_filepath)
+
+def load_controller(controller_name=None, scale=1.0, colour=0):
+    """Generate a NURBS curve in Maya according to the selected controller.
+    
+    Args:
+        controller_name (str): The name of the cotroller in the library.
+        scale (float): The scale amount of the new controller.
+        colour (int): The colour (according to Maya standards) of the new controller.
+
+    Returns:
+        curve (nt.Transform): The newlly created curve.
+    """
 
     if controller_name:
         control_to_create = controllers_dict.get(controller_name, None)
@@ -62,6 +99,9 @@ def generate_controller(controller_name=None, scale=1.0):
             curve = pm.curve(d=degree, p=cvs, k=knots, per=periodic, n=controller_name)
             curve.scale.set((scale, scale, scale))
             pm.makeIdentity(apply=True, t=False, r=False, s=True)
+            if colour:
+                curve.getShape().overrideEnabled.set(True)
+                curve.getShape().overrideColor.set(colour)
             return curve
         else:
             logger.error(f"Controller '{controller_name}' does not exist in the pickle file.")
@@ -72,3 +112,30 @@ def generate_controller(controller_name=None, scale=1.0):
 def controller_list():
     """Returns a list of stored controllers"""
     return sorted(controllers_dict.keys())
+
+
+def viewport_snip(name, crop_size=240):
+    """Capture an image of Maya's active viewport, crop it and save it as .png file."
+    
+    Args:
+        name (str): The filename of image file.
+        crop_size (int): The size, in pixels, of the final image. Currently set as square crop ratio.
+    """
+    view = omui.M3dView.active3dView()
+    img = om.MImage()
+
+    view.readColorBuffer(img, True)
+
+    icon_filename = os.path.join(constants.ICONS_FOLDER, f"{name}.png")
+    img.writeToFile(icon_filename, "png")
+
+    image = Image.open(icon_filename)
+    image_width, image_height = image.size
+
+    left = (image_width - crop_size) / 2
+    top = (image_height - crop_size) / 2
+    right = (image_width + crop_size) / 2
+    bottom = (image_height + crop_size) / 2
+
+    cropped_image = image.crop((left, top, right, bottom))
+    cropped_image.save(icon_filename)
